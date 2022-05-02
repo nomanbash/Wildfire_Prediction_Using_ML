c9f0b73ee7ec1d03c3be4d831c74c8fc889a9dd2
nomanbash <nomanbashirmalik@gmail.com>
2022-05-02 18:08:23 +0200

    Added several boxplots, barcharts. Spatial map pending

--- EDA.R
+++ EDA.R
@@ -17,7 +17,7 @@ nrow(wildfires)
 
 #turning time columns into POSIXct for better visualization
 wildfires <- wildfires %>%
-    mutate(discovery_date = 
+    mutate(discovery_date =
         as.POSIXct(disc_clean_date, format = "%m/%d/%Y"))
 wildfires$discovery_date
 wildfires <- wildfires %>%
@@ -33,22 +33,23 @@ wildfires$putout_time <-
     difftime(wildfires$contained_time, wildfires$discovery_time, units = "mins")
 wildfires$putout_time
 
-#we've now cleaned most of the data. 
-#Don't need disc_clean_date, disc_date_final, cont_date_final, 
+#we've now cleaned most of the data.
+#Don"t need disc_clean_date, disc_date_final, cont_date_final,
 #cont_clean_date, disc_date_pre, disc_pre_year, disc_pre_month
-#we also don't need wstation_usaf, wstation_byear, 
+#we also don"t need wstation_usaf, wstation_byear,
 #wstation_eyear and wstation_wban as these are identifiers
 
-wildfires_clean <- wildfires %>% 
-  select(-c('disc_clean_date', 'disc_date_final', 'cont_date_final', 
-  'cont_clean_date', 'disc_date_pre','disc_pre_year', 'disc_pre_month', 
-  'weather_file', 'fire_mag', 'wstation_usaf', 'wstation_byear', 
-  'wstation_eyear', 'wstation_wban'))
+wildfires_clean <- wildfires %>%
+  select(-c("disc_clean_date", "disc_date_final", "cont_date_final",
+  "cont_clean_date", "disc_date_pre", "disc_pre_year", "disc_pre_month",
+  "weather_file", "fire_mag", "wstation_usaf", "wstation_byear",
+  "wstation_eyear", "wstation_wban"))
 
 str(wildfires_clean)
 
 #barchart of fire sizes, heavily imbalanced, no As
-ggplot(wildfires_clean) + geom_bar(aes(fire_size_class), stat = "count") +
+ggplot(wildfires_clean) +
+  geom_bar(aes(fire_size_class), stat = "count") +
   coord_flip() +
   labs(title = "Histogram of Fire Sizes",
     x = "Fire Size Class",
@@ -62,30 +63,64 @@ wildfires_clean %>%
   ggplot(aes(x = reorder(stat_cause_descr, (count)), y = count)) +
   geom_bar(stat = "identity") +
   coord_flip() +
-  labs(title = "Causes of Fire", y = "Number of Fires", x = "Cause")
+  labs(title = "Causes of Fire", y = "Number of Fires", x = "Cause") +
+  geom_label(aes(label = count))
 
-#which cause is predominant in each class
-ggplot(wildfires_clean, aes(x = fire_size_class, fill = stat_cause_descr)) +
-  geom_bar(position = "fill", stat = "count")
-
-#we can see that that there's a lot of misisng/undefined. 
+#we can see that that there"s a lot of misisng/undefined.
 #Should count/remove them
 sum(wildfires_clean$stat_cause_descr == "Missing/Undefined")
+wildfires_clean <- wildfires_clean %>%
+  filter(stat_cause_descr != "Missing/Undefined")
 
-wildfires_clean <- wildfires_clean %>% filter(stat_cause_descr != "Missing/Undefined")
+#there's also only 58 "Structure" and 175 "Fireworks".
+#Maybe we should consider adding these to "Misc"
+#but let's see if they are overrepresented in one single class type first
 
-#replotting
 #which cause is predominant in each class
 ggplot(wildfires_clean, aes(x = fire_size_class, fill = stat_cause_descr)) +
-  geom_bar(position = "fill", stat = "count")
+  geom_bar(position = "fill", stat = "count") +
+  geom_label(aes(label = ..count..), stat = "count", position = "fill")
+
+wildfires_clean %>%
+  count(fire_size_class, stat_cause_descr)
+
+#fireworks and structure clearly aren't overrepresented anywhere.
+#This means they can be added to misc category
+wildfires_clean["stat_cause_descr"][wildfires_clean["stat_cause_descr"] == "Fireworks" | wildfires_clean["stat_cause_descr"] == "Structure"] <- "Miscellaneous"
+
+#plotting count of wildfires over months
+wildfires_clean %>%
+  mutate(Month = factor(discovery_month, levels = month.abb)) %>%
+  ggplot() + geom_bar(aes(Month, ..count..), stat = 'count')
+
+#now let's look at wildfire size
+wildfires_clean %>%
+  ggplot() + geom_histogram(aes(log(fire_size)))
+
+##now let's look at duration (or putout time)
+wildfires_clean %>%
+  ggplot() + geom_histogram(aes(log(as.numeric(putout_time))), na.rm = TRUE)
+
+#boxplots of fires per month
+wildfires_clean %>%
+  mutate(Month = factor(discovery_month, levels = month.abb)) %>%
+  group_by(Month, format(discovery_date, format = "%Y")) %>%
+  summarize(fires = n()) %>%
+  ggplot() + geom_boxplot(aes(x = Month, y = fires)) +
+  scale_y_continuous(labels = scales::comma)
+
+#let's see if cause has any correlation with area burnt
+wildfires_clean %>%
+  ggplot() + geom_boxplot(aes(stat_cause_descr, log(fire_size))) +
+  coord_flip()
+
+#lightning clearly burns more on average
+
+#lastly, let's see if a spatial representation yields some insights
+
 
 
 #correlation, Caution this takes long to run
-wildfires_clean %>% select(Temp_cont, Wind_cont, Hum_cont,Prec_cont, fire_size) %>% 
-  ggpairs()
-#Compose
-wildfires_clean$Vegetation<-as.character(wildfires_clean$Vegetation)
-wildfires_clean %>% select(Vegetation, fire_size_class) %>% 
-  ggpairs()
-wildfires_clean %>% select(stat_cause_descr, fire_size_class) %>% 
+wildfires_clean %>%
+  select(Temp_cont, Wind_cont, Hum_cont, Prec_cont, fire_size) %>%
   ggpairs()
